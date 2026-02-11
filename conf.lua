192e6f71de2e619a7bf0b6acb5c0f6bdfb18d80e
David Moulin <moulin.david@live.fr>
2026-02-11 20:10:04 +0100

    feat: asteroid game

--- conf.lua
+++ conf.lua
@@ -1,4 +1,5 @@
-function love.conf(t)
-    t.console = false
-    t.window.title = "TBH"
+function love.conf(app)
+    app.window.width = 1280
+    app.window.height = 720
+    app.window.title = "Asteroids"
 end
